246e1e748433ea3202aadead9bbb164a78ada17d
gavinarori <99688077+gavinarori@users.noreply.github.com>
2023-12-05 20:45:51 +0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="[color-scheme:dark]">
+      <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
+      <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
+            </div>
+        </body>
     </html>
   )
 }
